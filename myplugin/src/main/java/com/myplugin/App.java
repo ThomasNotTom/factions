c353658808456d6f068199392faf99bec2e129b0
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2024-10-17 11:23:29 +0100

    Create initial files

---
+++ myplugin/src/main/java/com/myplugin/App.java
@@ -0,0 +1,14 @@
+package com.myplugin;
+
+import org.bukkit.Bukkit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        Bukkit.broadcastMessage("HELLO, WORLD!");
+        System.out.println("Hello World!");
+    }
+}
